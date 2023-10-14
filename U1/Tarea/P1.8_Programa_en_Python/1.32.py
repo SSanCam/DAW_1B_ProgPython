1e1261e5c224f239e5f3a9096338839d10bc55e2
Sara Sanchez <ssancam391@g.educaand.es>
2023-10-14 16:57:11 +0200

    ejercicios 1.30-36

---
+++ U1/Tarea/P1.8_Programa_en_Python/1.32.py
@@ -0,0 +1,48 @@
+#
+# Ejercicio 1.32 - Lee dos números y crea la serie que los une de 1 en 1...
+#
+
+"""> Introduce un número: 4
+> Introduce otro: 8
+> 4-5-6-7-8
+
+> Introduce un número: 12
+> Introduce otro: 3
+> 3-4-5-6-7-8-9-10-11-12
+
+Inicio
+
+	Escribe "Introduce un número: "
+	Lee x
+	Escribe "Introduce otro: "
+	Lee y
+	
+	Si (x >= y) entonces
+		numIni = x
+		numFin = y
+	Sino
+		numIni = y
+		numFin = x
+		
+	Mientras (numIni <= numFin) hacer
+		Escribe numIni
+		Si (numIni != numFin) entonces
+			Escribe "-"
+                numIni = numIni + 1
+
+Fin"""
+x = int(input("Introduce un número: "))
+y = int(input("Introduce otro: "))
+
+if (x <= y):
+    numIni = x
+    numFin = y
+else:    
+    numIni = y
+    numFin = x
+
+while (numIni <= numFin):
+    
+    if (numIni != numFin):
+        print(numIni,end="-")
+        numIni += 1  
