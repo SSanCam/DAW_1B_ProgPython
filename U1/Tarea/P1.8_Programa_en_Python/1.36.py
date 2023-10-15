bf4b665bcb89122bf785af737f928432b1881a28
Sara S Camilleri <ssancam391@g.educaand.es>
2023-10-15 23:03:10 +0200

    fin ejercicios

--- U1/Tarea/P1.8_Programa_en_Python/1.36.py
+++ U1/Tarea/P1.8_Programa_en_Python/1.36.py
@@ -40,3 +40,23 @@ Inicio
 	Escribe "La media es " + media
 	
 Fin"""
+
+total = int(input("¿Cuántas notas vas a introducir? "))
+
+while total < 1 or total > 100:
+    print("Error - el número de notas debe ser un número entero entre 1 y 100")
+    total = int(input("¿Cuántas notas vas a introducir? "))
+
+print(f"Dame las {total} notas del curso:")
+
+media = 0
+cont = 1
+
+while cont <= total:
+    nota = float(input())
+    media = media + nota
+    cont = cont + 1
+
+media = media / total
+
+print(f"La media es {media:.2f}")
