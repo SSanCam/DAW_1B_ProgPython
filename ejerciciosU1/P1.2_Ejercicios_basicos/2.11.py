c46f99170cf223c40eb999cb1d415f01fb87c634
Sara S Camilleri <ssancam391@g.educaand.es>
2023-10-16 21:19:35 +0200

    correccion ej 1.5 y 1.11

--- ejerciciosU1/P1.2_Ejercicios_basicos/2.11.py
+++ ejerciciosU1/P1.2_Ejercicios_basicos/2.11.py
@@ -2,6 +2,6 @@
 todos los enteros desde 1 hasta n. La suma de los n primeros enteros positivos puede ser calculada de la siguiente forma:"""
 n = int(input("Introduce un numero entero: "))
 
-suma = (n * ( n + 1 )/2)
+suma = (n * (n + 1)/2)
 
 print(f"La suma de los primeros {n} enteros positivos es: {suma}")
