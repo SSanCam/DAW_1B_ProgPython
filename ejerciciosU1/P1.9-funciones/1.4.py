06f246946a740800300ebe9d9de156235fbae71b
Sara S Camilleri <ssancam391@g.educaand.es>
2023-10-16 19:33:22 +0200

    ejercicios de funciones

---
+++ ejerciciosU1/P1.9-funciones/1.4.py
@@ -0,0 +1,10 @@
+"""1.4 => NO recibe parámetros y retorna la temperatura convertida en grados Celsius. 
+Dentro de la función se pedirá al usuario los grados Farenheit.
+"""
+def conversor_temperatura ():
+    temperatura_celcius = float(input("¿Que temperatura, en grados Celsius, hace?: "))
+    conversion_fahrenheit = (temperatura_celcius * (9/5)) + 32
+
+    return print(f"{temperatura_celcius} grados Celsius es en grados Fahrenheit: {conversion_fahrenheit}")
+
+print(conversor_temperatura())
---
+++ ejerciciosU1/P1.9-funciones/1.5.py
@@ -0,0 +1,13 @@
+"""1.5 => recibe el importe del artículo sin iva y el tipo de iva a aplicar, pero no retorna nada, 
+sino que se imprime desde dentro de la función."""
+
+def precio_final () :
+    
+    precio_articulo = float(input("Cuanto cuesta el articulo?: "))
+    iva = float(input("Que porcentaje de IVA debe aplicarse?: "))
+    iva_aplicar = (iva / 100) * precio_articulo
+    importe_total = precio_articulo + iva_aplicar
+
+    return print(f"Precio de articulo: {precio_articulo}\nIVA a aplicar: {iva_aplicar}\nEl precio final es de: {importe_total} €.") 
+
+print(precio_final())
