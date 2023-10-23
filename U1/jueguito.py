4c339e8938e11663d35b93f6e94f9931e404371a
Sara S Camilleri <ssancam391@g.educaand.es>
2023-10-23 19:10:54 +0200

    tema2

--- U1/jueguito.py
+++ U1/jueguito.py
@@ -6,20 +6,24 @@ def jueguito ():
 
     while (numero_pedido < -100 or numero_pedido > 100):
         numero_pedido = int(input("ERROR.\nDebes introducir un numero entre -100 y 100.\nIngresa otro número: "))
-    
+    #INICIALIZO LAS VARIABLES
     suma = 0
     cont = 0 
     serie = ""
     
     while ( cont < numero_pedido):
+        #COMIENZA A CREARSE EL PRIMER NIVEL DE LA SERIE:
         suma += cont
         serie = serie + str(cont) + " + "
         cont += 1
-        
+                
         if (cont == numero_pedido):
+            #SI EL NIVEL ACABA LA SERIE, ACABA CON LA SUMA DE LOS NUMEROS Y UNA TABULACION PARA EL SIGUIENTE NIVEL, SI LO HUBIESE.
             suma += cont
             serie = serie + str(cont) + " = " + str(suma) + "\n"
             fin_serie = "0 + 0 = 0"
+            
+            #SI NO HA LLEGADO AL NIVEL FINAL, REINICIO VARIABLES Y ACTUALIZO EL VALOR DEL NUMERO PEDIDO
             numero_pedido = numero_pedido -1
             cont = 0
             suma = 0
