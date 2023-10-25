13f688b1e8eacf1f982ebe315ddca8acbb445888
Sara S Camilleri <ssancam391@g.educaand.es>
2023-10-25 16:07:25 +0200

    cambio nombre carpetas

--- U2/Ejercicios_U2/P2.1_SentenciasCondicionales/2.1.3/test_division.py
+++ U2/Ejercicios_U2/P2.1_SentenciasCondicionales/2.1.3/test_division.py
@@ -1,6 +1,16 @@
 import pytest
 from division import operacion_div
 
+@pytest.mark.parametrize(
+    "num1, num2, expected",
+    [
+      (10, 5, f"La división de {10}/{5} da como resultado: {10/5:.2f}"),
+      (20, "Eres mayor de edad."),
+      (16, "Eres menor de edad."),
+      (200, "Relája, Nosferatus.")
+    ]
+  )
+
 def test_operacion_div():
     assert operacion_div() == 2.00
     assert operacion_div() == ZeroDivisionError
