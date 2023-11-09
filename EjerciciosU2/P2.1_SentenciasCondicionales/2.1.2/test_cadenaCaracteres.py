c4bc17617a4c3730687272fd973f798ecb67581f
Sara S Camilleri <ssancam391@g.educaand.es>
2023-11-09 17:40:03 +0100

    09/11/23

--- EjerciciosU2/P2.1_SentenciasCondicionales/2.1.2/test_cadenaCaracteres.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from cadenaCaracteres import cadena
-
-def cadena():
-    assert cadena("AYtortilla") == "La contraseña es correcta."
-    assert cadena("ayTORTILLA") == "La contraseña es correcta."
-    assert cadena("tortillasincebolla") == "Contraseña incorrecta."
-    assert cadena("sintortilla") == "Contraseña incorrecta."
-    assert cadena("ahytortilla") == "Contraseña incorrecta."
