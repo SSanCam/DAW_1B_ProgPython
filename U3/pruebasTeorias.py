df22219d048d396ef82ade49c8e2446daa03ab2f
Sara S Camilleri <ssancam391@g.educaand.es>
2023-11-09 18:26:59 +0100

    sabe

---
+++ U3/pruebasTeorias.py
@@ -0,0 +1,13 @@
+colores = ["rojo", "verde"]
+
+print (colores)
+
+"""for color in colores:
+    color = "negro"
+    
+print(colores)
+
+for i in range(0, len(colores) -1):
+    colores[i] = "negro"
+    
+print(colores)"""
